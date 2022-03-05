e8afbb0b4d2e14be8c289342604294911048f5af
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-03-05 23:36:56 +0000

    Add barrier

---
+++ barrier/barrier.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type Barrier struct {
+	total int
+	count int
+	mutex *sync.Mutex
+	cond  *sync.Cond
+}
+
+func NewBarrier(size int) *Barrier {
+	lockToUse := &sync.Mutex{}
+	condToUse := sync.NewCond(lockToUse)
+	return &Barrier{size, size, lockToUse, condToUse}
+}
+
+func (barrier *Barrier) Wait() {
+	barrier.mutex.Lock()
+	barrier.count -= 1
+	if barrier.count == 0 {
+		barrier.count = barrier.total
+		barrier.cond.Broadcast()
+	} else {
+		barrier.cond.Wait()
+	}
+	barrier.mutex.Unlock()
+}
+
+const (
+	matrixSize = 250
+)
+
+var (
+	matrixA      = [matrixSize][matrixSize]int{}
+	matrixB      = [matrixSize][matrixSize]int{}
+	result       = [matrixSize][matrixSize]int{}
+	workStart    = NewBarrier(matrixSize + 1)
+	workComplete = NewBarrier(matrixSize + 1)
+)
+
+func generateRandomMatrix(matrix *[matrixSize][matrixSize]int) {
+	for row := 0; row < matrixSize; row++ {
+		for col := 0; col < matrixSize; col++ {
+			matrix[row][col] += rand.Intn(10) - 5
+		}
+	}
+}
+
+func workOutRow(row int) {
+	for {
+		workStart.Wait()
+		for col := 0; col < matrixSize; col++ {
+			for i := 0; i < matrixSize; i++ {
+				result[row][col] += matrixA[row][i] * matrixB[i][col]
+			}
+		}
+		workComplete.Wait()
+	}
+}
+
+func main() {
+	fmt.Println("Working...")
+	for row := 0; row < matrixSize; row++ {
+		go workOutRow(row)
+	}
+
+	start := time.Now()
+	for i := 0; i < 100; i++ {
+		generateRandomMatrix(&matrixA)
+		generateRandomMatrix(&matrixB)
+		workStart.Wait()
+		workComplete.Wait()
+	}
+	elapsed := time.Since(start)
+	fmt.Println("Done")
+	fmt.Printf("Processing took %s\n", elapsed)
+}
